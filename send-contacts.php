105d7326f6591b04f48585153792665b370d2ea6
Kris Hatcher <kris.hatcher@gmail.com>
2016-05-24 13:52:16 -0400

    Cleanup
    Adding a ZIP version of the plugin along with a name change to the file
    so that it follows WordPress naming conventions.

--- send-contacts.php
+++ send-contacts.php
@@ -1,5 +1,28 @@
 <?php
 
+/*
+Plugin Name: SendContacts
+Plugin URI:  http://www.smartdatasystems.net/wordpress/sendcontacts
+Description: Provides simple mechanism to allow site visitors to sign up for SendGrid-powered marketing lists.
+Version:     1.0
+Author:      Kris Hatcher
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+{Plugin Name} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+{Plugin Name} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {URI to Plugin License}.
+*/
+
 class SendContacts {
 	function register_menu() {
 		// Add a new submenu under Settings:
